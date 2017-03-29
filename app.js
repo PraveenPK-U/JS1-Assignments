6b39b2ed2dc44a5fd6a2f572c0900ae4de6579b7
PraveenPK-U <praveenkmr846@gmail.com>
2017-03-29 18:44:08 +0530

    JavaScript1-Assignments

---
+++ app.js
@@ -0,0 +1,36 @@
+﻿
+//Assignment1 Script
+var Name = "Praveen";
+var Age = 24;
+var DOB = "11-09-1992";
+var PlaceOfBirth = "Chennai";
+
+//Assignment2 Script
+var PersonalDetails = { Name: "Praveen", Age: "24", DOB: "11-09-1992", PlaceOfBirth: "Chennai" };
+
+//Assignment3 Script
+var Employee = {
+    Name: "Praveen", Age: "24", Salary: "10,000.00",
+    Address: { City: "Chennai", State: "TamilNadu", PinCode: "600048" }
+};
+
+//Assignment4 Script
+
+var Employee2 = {
+    Name: "Hari", Age: "24", Salary: "35,000.00",
+    Address: { City: "Chennai", State: "TamilNadu", PinCode: "600048" }
+};
+var Employee3 = {
+    Name: "Dilip", Age: "24", Salary: "20,000.00",
+    Address: { City: "Chennai", State: "TamilNadu", PinCode: "600048" }
+};
+var Employee4 = {
+    Name: "Karthick", Age: "24", Salary: "15,000.00",
+    Address: { City: "Chennai", State: "TamilNadu", PinCode: "600048" }
+};
+var Employee5 = {
+    Name: "Akil", Age: "24", Salary: "30,000.00",
+    Address: { City: "Chennai", State: "TamilNadu", PinCode: "600048" }
+};
+
+var EmployeeArray = [Employee, Employee2, Employee3, Employee4, Employee5];
